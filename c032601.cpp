f71197cdafd2b06957af994de321e2f22d86b526
金井則通(Norimichi Kanai) <k.norimichi9999@i.softbank.jp>
2016-03-26 17:05:42 +0900

    Create c032601.cpp

---
+++ c032601.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+#include "glibw32.h"
+
+const int NUMBER = 360;
+const double PI = 3.141593;
+
+main()
+{
+   int i, j;
+   double t, p, x, y, z, r, R, N;
+   ginit(500, 500, WHITE);
+   GRAPH g;
+   g.window(-PI, -PI, PI, PI);
+   g.line(-PI, 0, PI, 0);  // x
+   g.line(0, PI, 0, -PI);  // y
+   g.setcolor(BLACK);
+   r = 1.0;
+   R = 2.0;
+   i = 0;
+   while(i < NUMBER)
+   {
+       t = i * (PI / (NUMBER / 2));
+       j = 0;
+       while(j < NUMBER)
+       {
+           p = j * (PI / (NUMBER / 2));
+           
+           x = R * cos(t) + r * cos(p) * cos(t); //Wikipedia
+           y = R * sin(t) + r * cos(p) * sin(t);
+           z = r * sin(p);
+           
+           g.pset(x, y);
+           j = j + 1;
+       }
+       i = i + 1;
+   }
+   gend();
+}
+// cd c:\mingw-jp-20040217\bin
+// g++ c032601.cpp libglibw32-3.a -IC:\mingw-jp-20040217\include\ -LC:\mingw-jp-20040217\lib\ -IC:\Glibw32\ -LC:\Glibw32\ -luser
+// 32 -lgdi32
+// rename a.exe c032601.*
